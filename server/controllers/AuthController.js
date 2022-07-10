bbd61e107a57fe65e60419a0629c506ca31ecf22
amaboh <amabocanon@gmail.com>
2022-07-10 11:23:17 +0100

    bug fix server error 500

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,14 +1,16 @@
 import UserModel from "../models/UserModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
 
 // Register new user
 
 export const registerUser = async (req, res, next) => {
+  const { username } = req.body;
   try {
-    const { username } = req.body;
+    const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
-    const oldUser = await UserModel.find({ username });
+    const oldUser = await UserModel.findOne({ username });
 
     if (oldUser)
       return res.status(400).json({ message: "User already exists" });
@@ -28,42 +30,33 @@ export const registerUser = async (req, res, next) => {
       { expiresIn: "1h" }
     );
 
-    res.status(200).json(user,token);
+    res.status(200).json({ user, token });
   } catch (err) {
     res.status(500).json(err);
   }
 };
 
 // Login User
-export const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ username: username});
-        if (!user) return next(createError(404, "User not found"));
+export const login = async (req, res,next) => {
+  try {
+    const user = await UserModel.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
 
-        const isPasswordCorrect = await bcrypt.compare(
-            req.body.password, 
-            user.password
-        );
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-        if (!isPasswordCorrect)
-        return next(createError(400, "Wrong password or username!"));
+    // implementing authorization with JWT
+   
 
-         // implementing authorization with JWT 
-        const token = jwt.sign(
-            { id: user._id, isAdmin: user.isAdmin },
-            process.env.JWT_KEY,
-            {expiresIn: "2h"}
-        );
-
-        const { password, isAdmin, ...otherDetails } = user._doc;
-        res
-        .cookie("access_token", token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json({details: { ...otherDetails },isAdmin})
-    } catch (err) {
-        res.status(500).json(err);
-
-    }
-}
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+      .status(200)
+      .json({ details: { ...otherDetails }, isAdmin });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from "mongoose"
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 import cors from "cors";
 import chatRoute from "./routes/chatRoute.js"
 import userRoute from "./routes/userRoute.js"
@@ -9,6 +10,16 @@ import authRoute from "./routes/authRoute.js"
 dotenv.config()
 
 const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"],
+  })
+);
 
 const PORT = process.env.PORT;
 
